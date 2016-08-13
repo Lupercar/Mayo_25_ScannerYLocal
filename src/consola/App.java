3645ed11255c92fec4c607d7261d46d40a5e61af
Lupercar <hotolete@gmail.com>
2016-08-13 23:43:42 +0200

    Scanner de String

--- src/consola/App.java
+++ src/consola/App.java
@@ -1,5 +1,7 @@
 package consola;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class App {
@@ -8,9 +10,28 @@ public class App {
 		
 		Scanner s = new Scanner("Mi  casa es de color rojo");
 		
-		while(s.hasNext())
-			System.out.println(s.next());
+//		while(s.hasNext())
+//			System.out.println(s.next());
 		
 		s.close();
+		
+//		La clase Scanner tiene en cuenta la configuración regional del usuario. 
+		Scanner s2 = new Scanner("1 Leche 2,00");
+		
+		int id = 0;
+		if(s2.hasNextInt()) 
+			id = s2.nextInt(); 
+		
+		String producto = s2.next(); 
+		double precio = s2.nextDouble(); 
+		
+		s2.close(); 
+		
+//		No tiene en cuenta la configuración regional del usuario.
+		System.out.println("Código: " + id +
+											" Producto: " + producto +
+											" Precio: " + precio);
+		
+
 	}
 }//fin class consola.App
